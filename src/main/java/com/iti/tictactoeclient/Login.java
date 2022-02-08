e18634b39b0443e2dca230f154e1d45aa3a3923c
Mark1George <markogeorge94@gmail.com>
2022-02-09 01:16:02 +0200

    login Gui

--- src/main/java/com/iti/tictactoeclient/Login.java
+++ src/main/java/com/iti/tictactoeclient/Login.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class TicTacToeClient extends Application {
+public class Login extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(TicTacToeClient.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Login.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
     }
