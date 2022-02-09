8f9ea06709027335b0fe2b273561649074c40074
amllotfy <amllotfy136@gmail.com>
2022-02-10 00:55:35 +0200

    view

--- src/main/java/com/iti/tictactoeclient/TicTacToeClient.java
+++ src/main/java/com/iti/tictactoeclient/TicTacToeClient.java
@@ -2,6 +2,7 @@ package com.iti.tictactoeclient;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,7 +12,7 @@ public class TicTacToeClient extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(TicTacToeClient.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 700, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
