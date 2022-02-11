c9c65588a37328467f046af9bdcd756145b4c19b
Mark1George <markogeorge94@gmail.com>
2022-02-11 16:51:05 +0200

    Gui- changes

---
+++ src/main/java/com/iti/tictactoeclient/controllers/RegisterController.java
@@ -0,0 +1,45 @@
+package com.iti.tictactoeclient.controllers;
+
+import com.iti.tictactoeclient.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RegisterController implements Initializable {
+
+    @FXML
+    private ImageView backgroundimg;
+
+    @FXML
+    private TextField FirstNameTxt;
+
+    @FXML
+    private PasswordField PasswordTxt;
+
+    @FXML
+    private Button RegiterButton;
+
+    @FXML
+    private TextField UserNameTxt;
+
+    @FXML
+    protected void onActionRegister() {
+        Main.openloginView();
+    }
+
+    @FXML
+    public void initialize (URL url, ResourceBundle resourceBundle){
+        File backfile = new File("images/7.png");
+        Image background = new Image(backfile.toURI().toString());
+        backgroundimg.setImage(background);
+
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,14 @@
+//module com.iti.tictactoeclient {
+//    requires javafx.controls;
+//    requires javafx.fxml;
+//
+//
+//    opens com.iti.tictactoeclient to javafx.fxml;
+//    exports com.iti.tictactoeclient;
+//    //exports com.iti.tictactoeclient.Controller;
+//    opens com.iti.tictactoeclient.Controller to javafx.fxml;
+//}
+
 module com.iti.tictactoeclient {
     requires javafx.controls;
     requires javafx.fxml;
@@ -5,6 +16,7 @@ module com.iti.tictactoeclient {
 
     opens com.iti.tictactoeclient to javafx.fxml;
     exports com.iti.tictactoeclient;
-    exports com.iti.tictactoeclient.Controller;
-    opens com.iti.tictactoeclient.Controller to javafx.fxml;
+    exports com.iti.tictactoeclient.controllers;
+    opens com.iti.tictactoeclient.controllers to javafx.fxml;
 }
+
