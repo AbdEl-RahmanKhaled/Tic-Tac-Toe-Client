1a6b89fb1b285ec3ff4161586b068b9aeaa2f96c
nouranhamdy <nouranhamdy4555@gmail.com>
2022-02-17 16:33:14 +0200

    User & Position

---
+++ src/main/java/com/iti/tictactoeclient/models/User.java
@@ -0,0 +1,32 @@
+package com.iti.tictactoeclient.models;
+
+public class User extends Credentials {
+    private int u_id;
+    private String name;
+    private int points;
+
+    public void setId(int Id) {
+        u_id = Id;
+    }
+
+    public void setName(String Name) {
+        name = Name;
+    }
+
+    public void setPoints(int Points) {
+        points = Points;
+    }
+
+    public int getId() {
+        return u_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public int getPoints() {
+        return points;
+    }
+}
